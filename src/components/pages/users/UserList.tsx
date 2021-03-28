7171117d1b06a41593c56b3e7c1e84dbba32eeb2
azumag <tsubasa.azumagakito@gmail.com>
2021-03-28 22:59:11 +0900

    add user list page

---
+++ src/components/pages/users/UserList.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { fireauth } from "utils/firebase";
+import { toast } from "react-toastify";
+
+class UserList extends React.Component<{}> {
+  constructor(props: {}) {
+    super(props);
+  }
+
+  render(): JSX.Element {
+    return (
+      <div>test</div>
+    );
+  }
+}
+
+export default UserList;
