0c901af18dca4d46406b3ba7e69919d2bc9579cd
azumag <tsubasa.azumagakito@gmail.com>
2021-03-28 23:55:04 +0900

    add user list display names

--- src/components/pages/users/UserList.tsx
+++ src/components/pages/users/UserList.tsx
@@ -1,15 +1,55 @@
 import React from "react";
-import { fireauth } from "utils/firebase";
+import { firestore } from "utils/firebase";
 import { toast } from "react-toastify";
 
-class UserList extends React.Component<{}> {
+type UserListState = {
+  list: any[];
+};
+
+class UserList extends React.Component<{}, UserListState> {
   constructor(props: {}) {
     super(props);
+    this.state = {
+      list: []
+    }
+  }
+
+  getData = async () => {
+    const ref = firestore.collection('users');
+    const snapshots = await ref.get();
+    const docs = snapshots.docs.map(doc => doc.data());
+    this.setState({ list: docs, });
+  }
+
+  componentDidMount = async () => {
+    await this.getData();
+  }
+
+  componentWillUnmount = () => {
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    this.getData();
+  }
+
+  ItemList(props) {
+    const users = props.users;
+    const listItems = users.map((user) =>
+      <li>{user.name}</li>
+    );
+    return (
+      <ul>
+        {listItems}
+      </ul>
+    );
   }
 
   render(): JSX.Element {
     return (
-      <div>test</div>
+      <div>
+        <h1>USER LIST</h1>
+        <this.ItemList users={this.state.list}/>
+      </div>
     );
   }
 }
